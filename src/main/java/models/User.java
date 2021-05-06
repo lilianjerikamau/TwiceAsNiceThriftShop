3adf730a39f92eb6b2b463ec6910e15693b62d65
KevinMokoro <klosvoke1@gmail.com>
2021-05-06 04:20:53 +0300

    Add method to log in user to their profile,

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -63,6 +63,18 @@ public class User {
         return password;
     }
 
+//this method logs in user into their profile,it can have product items specific to them
+    public static User login(String name, String password) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM users where name=:name and password=:password";
+            User user = con.createQuery(sql)
+                    .addParameter("name", name)
+                    .addParameter("password", password)
+                    .executeAndFetchFirst(User.class);
+            return user;
+        }
+    }
+
 
 
 }
